0712f93593fd18919fce35ba235b374fe01b54da
lordxist <julian.jabs@web.de>
2010-04-22 22:32:07 +0200

    bugfix: player model validations

--- generators/quest/templates/player_model.rb
+++ generators/quest/templates/player_model.rb
@@ -1,6 +1,11 @@
 class <%= player_class_name %> < ActiveRecord::Base
   belongs_to :<%= singular_name %>
-  
+
+  with_options :if => "quest_id_changed?" do |e|
+    e.validate :no_early_<%= singular_name %>_joins
+    e.validate :<%= singular_name %>_must_not_have_started
+  end
+
   def no_early_<%= singular_name %>_joins
     <%= singular_name %>.ready_for_join? || errors.add_to_base("You can't join <%= plural_name %> so early.")
   end
